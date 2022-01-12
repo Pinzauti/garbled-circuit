aa3f668613a40976b78b809fca3c8d3ed6497647
Francesco Pinzauti <francescopinzauti@gmail.com>
2022-01-12 21:56:15 +0100

    refactor: removed unused parser

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
+"""
+Francesco Pinzauti
+"""
+import argparse
 from yao.garbler import Alice, Bob, LocalTest, logging
 
 
 def main(
-    party,
-    circuit_path="circuit/add.json",
-    oblivious_transfer=True,
-    print_mode="circuit",
-    loglevel=logging.WARNING,
+        party,
+        circuit_path="circuit/add.json",
+        print_mode="circuit",
+        loglevel=logging.WARNING,
 ):
+    """
+    d
+    """
     logging.getLogger().setLevel(loglevel)
 
     if party == "alice":
-        alice = Alice(circuit_path, oblivious_transfer=oblivious_transfer)
+        alice = Alice(circuit_path, oblivious_transfer=True)
         alice.start()
     elif party == "bob":
-        bob = Bob(oblivious_transfer=oblivious_transfer)
+        bob = Bob(oblivious_transfer=True)
         bob.listen()
     elif party == "local":
         local = LocalTest(circuit_path, print_mode=print_mode)
@@ -23,51 +29,40 @@ def main(
         logging.error(f"Unknown party '{party}'")
 
 
-if __name__ == '__main__':
-    import argparse
+def init():
+    """
+    c
+    """
+    loglevels = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL
+    }
 
-    def init():
-        loglevels = {
-            "debug": logging.DEBUG,
-            "info": logging.INFO,
-            "warning": logging.WARNING,
-            "error": logging.ERROR,
-            "critical": logging.CRITICAL
-        }
+    parser = argparse.ArgumentParser(description="Run Yao protocol.")
+    parser.add_argument("party",
+                        choices=["alice", "bob", "local."],
+                        help="The yao party to run")
+    parser.add_argument(
+        "-m",
+        metavar="mode",
+        choices=["circuit", "table"],
+        default="circuit",
+        help="The print mode for local tests (default 'circuit').")
+    parser.add_argument("-l",
+                        "--loglevel",
+                        metavar="level",
+                        choices=loglevels.keys(),
+                        default="warning",
+                        help="The log level (default 'warning').")
 
-        parser = argparse.ArgumentParser(description="Run Yao protocol.")
-        parser.add_argument("party",
-                            choices=["alice", "bob", "local"],
-                            help="the yao party to run")
-        parser.add_argument(
-            "-c",
-            "--circuit",
-            metavar="circuit.json",
-            default="circuit/add.json",
-            help=("the JSON circuit file for alice and local tests"),
-        )
-        parser.add_argument("--no-oblivious-transfer",
-                            action="store_true",
-                            help="disable oblivious transfer")
-        parser.add_argument(
-            "-m",
-            metavar="mode",
-            choices=["circuit", "table"],
-            default="circuit",
-            help="the print mode for local tests (default 'circuit')")
-        parser.add_argument("-l",
-                            "--loglevel",
-                            metavar="level",
-                            choices=loglevels.keys(),
-                            default="warning",
-                            help="the log level (default 'warning')")
+    main(
+        party=parser.parse_args().party,
+        print_mode=parser.parse_args().m,
+        loglevel=loglevels[parser.parse_args().loglevel],
+    )
 
-        main(
-            party=parser.parse_args().party,
-            circuit_path=parser.parse_args().circuit,
-            oblivious_transfer=not parser.parse_args().no_oblivious_transfer,
-            print_mode=parser.parse_args().m,
-            loglevel=loglevels[parser.parse_args().loglevel],
-        )
 
-    init()
+init()
