122ef8669a43b9fd1d5e3d171d765e524d08cb37
Francesco Pinzauti <francescopinzauti@gmail.com>
2022-01-13 10:39:44 +0100

    refactor: removed loglevel

--- src/main.py
+++ src/main.py
@@ -76,23 +76,14 @@ class Sender(Alice):
             w: self._get_encr_bits(pbits[w], key0, key1)
             for w, (key0, key1) in keys.items() if w in b_wires
         }
-
         print(f"======== {circuit['id']} ========")
-
         bits_a = list(f"{sum(self.data_sender):b}".zfill(8))
         bits_a = [int(i) for i in bits_a]
-
-        # Map Alice's wires to (key, encr_bit)
         for i, _ in enumerate(a_wires):
             a_inputs[a_wires[i]] = (keys[a_wires[i]][bits_a[i]],
                                     pbits[a_wires[i]] ^ bits_a[i])
-
-        # Send Alice's encrypted inputs and keys to Bob
         result = self.ot.get_result(a_inputs, b_keys)
-
-        # Format output
         str_result = ''.join([str(result[w]) for w in outputs])
-
         print(f'The sum of Alice is {sum(self.data_sender)} '
               f'and the sum of Bob is {sum(self.data_receiver)}.')
         print(f'{verify_output(sum(self.data_sender), sum(self.data_receiver), int(str_result, 2))}'
@@ -126,14 +117,6 @@ def init():
     """
     TODO
     """
-    loglevels = {
-        "debug": logging.DEBUG,
-        "info": logging.INFO,
-        "warning": logging.WARNING,
-        "error": logging.ERROR,
-        "critical": logging.CRITICAL
-    }
-
     parser = argparse.ArgumentParser(description="Run Yao protocol.")
     parser.add_argument("party",
                         choices=["alice", "bob", "local"],
@@ -144,17 +127,9 @@ def init():
         choices=["circuit", "table"],
         default="circuit",
         help="The print mode for local tests (default 'circuit').")
-    parser.add_argument("-l",
-                        "--loglevel",
-                        metavar="level",
-                        choices=loglevels.keys(),
-                        default="warning",
-                        help="The log level (default 'warning').")
-
     main(
         party=parser.parse_args().party,
         print_mode=parser.parse_args().m,
-        loglevel=loglevels[parser.parse_args().loglevel],
     )
 
 
