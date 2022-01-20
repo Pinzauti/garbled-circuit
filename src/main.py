c64972bef749b01145e1754306f4419711a88db7
Francesco Pinzauti <francescopinzauti@gmail.com>
2022-01-20 16:27:15 +0100

    feat: write output file

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ TODO
 """
 import argparse
 from yao.garbler import Alice, Bob, LocalTest
-from utils import read_input, verify_output, ALICE_DATA_PATH, BOB_DATA_PATH
+from utils import read_input, verify_output, write_to_file, ALICE_DATA_PATH, BOB_DATA_PATH
 
 
 class Receiver(Bob):
@@ -22,17 +22,16 @@ class Receiver(Bob):
         circuit, pbits_out = entry["circuit"], entry["pbits_out"]
         garbled_tables = entry["garbled_tables"]
         b_wires = circuit.get("bob", [])
-
-        print(f"Received {circuit['id']}")
-        print(f'The sum of Bob is {sum(self.data_bob)}')
         bits_b = list(f"{sum(self.data_bob):b}".zfill(8))
         bits_b = [int(i) for i in bits_b]
-
         b_inputs_clear = {
             b_wires[i]: bits_b[i]
             for i in range(len(b_wires))
         }
-
+        print(f"Received {circuit['id']}")
+        write_to_file(clear=True)
+        write_to_file(f'Bob\'s input set sum is {sum(self.data_bob)}\n')
+        write_to_file(f'Bob\'s message for Alice is {"".join(map(str, pbits_out.values()))}\n')
         self.ot.send_result(circuit, garbled_tables, pbits_out,
                             b_inputs_clear)
 
@@ -59,7 +58,6 @@ class Sender(Alice):
             w: self._get_encr_bits(pbits[w], key0, key1)
             for w, (key0, key1) in keys.items() if w in b_wires
         }
-        print(f"======== {circuit['id']} ========")
         bits_a = list(f"{sum(self.data_alice):b}".zfill(8))
         bits_a = [int(i) for i in bits_a]
         for i, _ in enumerate(a_wires):
@@ -67,8 +65,10 @@ class Sender(Alice):
                                     pbits[a_wires[i]] ^ bits_a[i])
         result = self.ot.get_result(a_inputs, b_keys)
         str_result = ''.join([str(result[w]) for w in outputs])
-        print(f'The sum of Alice is {sum(self.data_alice)}')
+        write_to_file(f'Alice\'s input set sum is {sum(self.data_alice)}\n')
+        write_to_file(f'Alice\'s message for Bob is {str_result}\n')
         verify_output(int(str_result, 2))
+        print('Communication completed, all the information are in the file.')
 
 
 def main(party, circuit_path='circuit/add.json'):
--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,17 @@ def read_input(path):
     return input_data
 
 
+def write_to_file(message='', clear=False):
+    """
+    TODO
+    """
+    path = 'output/result.txt'
+    with open(path, 'a', encoding='UTF-8') as file:
+        file.write(message)
+        if clear:
+            file.truncate(0)
+
+
 def verify_output(result):
     """
     TODO
@@ -24,6 +35,6 @@ def verify_output(result):
     sender_data = read_input(ALICE_DATA_PATH)
     receiver_data = read_input(BOB_DATA_PATH)
     if (sum(sender_data) + sum(receiver_data)) == result:
-        print(f'The sum is correct and it is {result}.')
+        write_to_file(f'The sum is correct and it is {result}.')
     else:
-        print(f'The sum is {result} and it is incorrect.')
+        write_to_file('The sum is {result} and it is incorrect.')
